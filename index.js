a35f1a37977d5c9afe5d59667ba4214e0f8d87ad
hellolima <gabrielladelimadev@gmail.com>
2025-06-13 08:08:01 -0300

    Commit 3 - Mais Extract Functions

--- index.js
+++ index.js
@@ -1,57 +1,80 @@
 const { readFileSync } = require('fs');
 
-function gerarFaturaStr (fatura, pecas) {
-    let totalFatura = 0;
-    let creditos = 0;
-    let faturaStr = `Fatura ${fatura.cliente}\n`;
-    const formato = new Intl.NumberFormat("pt-BR",
-                          { style: "currency", currency: "BRL",
-                            minimumFractionDigits: 2 }).format;
+function gerarFaturaStr(fatura, pecas) {
+  let totalFatura = 0;
+  let creditos = 0;
+  let faturaStr = `Fatura ${fatura.cliente}\n`;
+  //const formato = new Intl.NumberFormat("pt-BR",
+    //{
+     // style: "currency", currency: "BRL",
+     // minimumFractionDigits: 2
+    //}).format;
 
-    function getPeca(apresentacao) {
-      return pecas[apresentacao.id];
-    }
-  
-    function calcularTotalApresentacao(apre){
-      let total = 0;
-      switch (getPeca(apre).tipo) {
-        case "tragedia":
-          total = 40000;
-          if (apre.audiencia > 30) {
-            total += 1000 * (apre.audiencia - 30);
-          }
-          break;
-        case "comedia":
-          total = 30000;
-          if (apre.audiencia > 20) {
-             total += 10000 + 500 * (apre.audiencia - 20);
-          }
-          total += 300 * apre.audiencia;
-          break;
-        default:
-            throw new Error(`Peça desconhecia: ${getPeca(apre).tipo}`);
-        }
-    
-        // créditos para próximas contratações
-        creditos += Math.max(apre.audiencia - 30, 0);
-        if (getPeca(apre).tipo === "comedia") 
-           creditos += Math.floor(apre.audiencia / 5);
-    
-        // mais uma linha da fatura
-        faturaStr += `  ${getPeca(apre).nome}: ${formato(total/100)} (${apre.audiencia} assentos)\n`;
-        totalFatura += total;
-    }
-                      
-    for (let apre of fatura.apresentacoes) {
-      // const peca = getPeca(apre);
-      calcularTotalApresentacao(apre);
-    }
-
-
-    faturaStr += `Valor total: ${formato(totalFatura/100)}\n`;
-    faturaStr += `Créditos acumulados: ${creditos} \n`;
-    return faturaStr;
+  function formatarMoeda(valor) {
+    return new Intl.NumberFormat("pt-BR",
+      {
+        style: "currency", currency: "BRL",
+        minimumFractionDigits: 2
+      }).format(valor / 100);
   }
+
+  function getPeca(apresentacao) {
+    return pecas[apresentacao.id];
+  }
+
+  function calcularTotalApresentacao(apre) {
+    let total = 0;
+    switch (getPeca(apre).tipo) {
+      case "tragedia":
+        total = 40000;
+        if (apre.audiencia > 30) {
+          total += 1000 * (apre.audiencia - 30);
+        }
+        break;
+      case "comedia":
+        total = 30000;
+        if (apre.audiencia > 20) {
+          total += 10000 + 500 * (apre.audiencia - 20);
+        }
+        total += 300 * apre.audiencia;
+        break;
+      default:
+        throw new Error(`Peça desconhecia: ${getPeca(apre).tipo}`);
+    }
+
+    // créditos para próximas contratações
+    //creditos += Math.max(apre.audiencia - 30, 0);
+    //if (getPeca(apre).tipo === "comedia") 
+    //creditos += Math.floor(apre.audiencia / 5);
+
+    // mais uma linha da fatura
+    faturaStr += `  ${getPeca(apre).nome}: ${formatarMoeda(total)} (${apre.audiencia} assentos)\n`;
+    totalFatura += total;
+  }
+
+  function calcularCredito(apre) {
+    let creditos = 0;
+    creditos += Math.max(apre.audiencia - 30, 0);
+    if (getPeca(apre).tipo === "comedia")
+      creditos += Math.floor(apre.audiencia / 5);
+    return creditos;
+  }
+
+  for (let apre of fatura.apresentacoes) {
+    // const peca = getPeca(apre);
+    calcularTotalApresentacao(apre);
+    creditos += calcularCredito(apre);
+  }
+
+
+
+
+
+
+  faturaStr += `Valor total: ${formatarMoeda(totalFatura)}\n`;
+  faturaStr += `Créditos acumulados: ${creditos} \n`;
+  return faturaStr;
+}
 
 const faturas = JSON.parse(readFileSync('./faturas.json'));
 const pecas = JSON.parse(readFileSync('./pecas.json'));
